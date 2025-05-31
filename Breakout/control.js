59ea0ba644b70369de19846fa04db6ce15e7dbea
JaxiKun <gmthomas@email.neit.edu>
2025-05-31 15:01:37 -0400

    controls are shown on the side

--- Breakout/control.js
+++ Breakout/control.js
@@ -2,10 +2,10 @@ let leftPressed = false;
 let rightPressed = false;
 
 window.addEventListener("keydown", function(e) {
-    if(e.key === "ArrowLeft") leftPressed = true;
-    if(e.key === "ArrowRight") rightPressed = true;
+    if(e.key === "ArrowLeft" || e.key === "a" || e.key === "A") leftPressed = true;
+    if(e.key === "ArrowRight" || e.key === "d" || e.key === "D") rightPressed = true;
 });
 window.addEventListener("keyup", function(e) {
-    if(e.key === "ArrowLeft") leftPressed = false;
-    if(e.key === "ArrowRight") rightPressed = false;
+    if(e.key === "ArrowLeft" || e.key === "a" || e.key === "A") leftPressed = false;
+    if(e.key === "ArrowRight" || e.key === "d" || e.key === "D") rightPressed = false;
 });
